ef8377d043ec62ef3af11530e5f26e7a9faf8330
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-12-16 23:15:20 +0100

    added add addresses features + reorganised elements

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -13,6 +13,11 @@ const UserInfo = () => {
     const [Message, setMessage] = useState(''); 
     const [passwords, setPasswords] = useState({ oldPassword: '', newPassword: '' });
     const { authUser, logout } = useContext(AuthContext);
+
+    const [addresses, setAddresses] = useState([]);
+    const [editingAddressId, setEditingAddressId] = useState(null);
+    const [editedAddress, setEditedAddress] = useState({});
+
     const navigate = useNavigate(); 
     const [paymentInfo, setPaymentInfo] = useState({
         type: '',
@@ -20,13 +25,36 @@ const UserInfo = () => {
         threeDigitCode: '',
         user_id: authUser.id
     });
+
+    const [newAddress, setNewAddress] = useState({
+        street: '',
+        city: '',
+        zipCode: '',
+        country: ''
+    });
+
     const [showPaymentForm, setShowPaymentForm] = useState(false);
     const [messageType, setMessageType] = useState('');
 
     const backendUrl = "http://127.0.0.1:3000";
 
+    const loadAddresses = () => {
+        if (authUser && authUser.id) {
+            axios.get(`${backendUrl}/users/${authUser.id}/addresses`)
+                .then(response => {
+                    setAddresses(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                    // possible error msg (afficher à l'utilisateur)
+                });
+        }
+    };
+
     useEffect(() => {
         if (authUser && authUser.id) {
+            loadAddresses();
+
             //axios.get(`http://localhost:3000/users/${authUser.id}`)
             axios.get(`${backendUrl}/users/${authUser.id}`)
 
@@ -147,19 +175,80 @@ const UserInfo = () => {
         }
     };
 
+    //Handle Addresses
+    const handleAddAddress = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${backendUrl}/users/${authUser.id}/addresses`, newAddress);
+            if (response.status === 201) {
+                // Ajoutez la nouvelle adresse à votre état local 'addresses'
+                setAddresses([...addresses, response.data]);
+                // Réinitialisez le formulaire
+                setNewAddress({ street: '', city: '', zipCode: '', country: '' });
+            }
+        } catch (error) {
+            console.error('Erreur lors de l\'ajout de l\'adresse', error);
+            // Gérez l'erreur ici
+        }
+    };
+    const startEditing = (address) => {
+        setEditingAddressId(address.id);
+        setEditedAddress({...address});
+    };
+    const handleUpdateAddress = async (addressId, updatedAddress) => {
+        try {
+            const response = await axios.put(`${backendUrl}/addresses/${addressId}`, updatedAddress);
+            if (response.status === 200) {
+                // Mise à jour de l'état local avec la nouvelle adresse
+                setAddresses(addresses.map(address => address.id === addressId ? {...address, ...updatedAddress} : address));
+            }
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour de l\'adresse', error);
+            // Gérez l'erreur ici
+        }
+    };
+    const saveAddress = async (addressId) => {
+        try {
+            // Utilisation de handleUpdateAddress pour envoyer les modifications
+            await handleUpdateAddress(addressId, editedAddress);
+    
+            // Réinitialisation de l'état d'édition
+            setEditingAddressId(null);
+            setEditedAddress({});
+        } catch (error) {
+            console.error('Erreur lors de la sauvegarde de l\'adresse', error);
+            // Gérez l'erreur ici
+        }
+    };
+    const handleDeleteAddress = async (addressId) => {
+        try {
+            const response = await axios.delete(`${backendUrl}/addresses/${addressId}`);
+            if (response.status === 204) {
+                // Supprimer l'adresse de l'état local
+                setAddresses(addresses.filter(address => address.id !== addressId));
+            }
+        } catch (error) {
+            console.error('Erreur lors de la suppression de l\'adresse', error);
+            // Gérez l'erreur ici
+        }
+    };
+
     if (!userInfo) return <div>Loading...</div>;
 
     return (
         <div>
             <div className="container-user" >
+                <div> </div>
                 <h2>User Information</h2>
+                <div> </div>
             </div>
         <div className="container-user">
             <div className="info-column">
                 {editableMode ? (
                     <form onSubmit={handleUpdate}>
                         {/* User info inputs */}
-                        <div>
+                    <div>
+                    
                     <label htmlFor="username">Username:</label>
                     <input
                         type="text"
@@ -203,6 +292,7 @@ const UserInfo = () => {
                     </form>
                 ) : (
                     <div>
+                        <h3>Personal information</h3>
                         <p><b>Username:</b> {userInfo.username}</p>
                         <p><b>First and Last Name:</b> {userInfo.fullName}</p>
                         <p><b>Email Address:</b> {userInfo.email}</p>
@@ -210,9 +300,106 @@ const UserInfo = () => {
                         <button onClick={handleToggleEditableMode}>Update</button>
                     </div>
                 )}
-            </div>
+               
+                
             
+            </div>
+            {/* addresses part */}
+            
+            <div className="addresses">
+            {addresses.map((address) => (
+            <div key={address.id} className="address">
+                {editingAddressId === address.id ? (
+                    <div>
+                        <input
+                        type="text"
+                        value={editedAddress.street || address.street}
+                        onChange={e => setEditedAddress({...editedAddress, street: e.target.value})}
+                        />
+                        <input
+                            type="text"
+                            value={editedAddress.city || address.city}
+                            onChange={e => setEditedAddress({...editedAddress, city: e.target.value})}
+                        />
+                        <input
+                            type="text"
+                            value={editedAddress.zipCode || address.zipCode}
+                            onChange={e => setEditedAddress({...editedAddress, zipCode: e.target.value})}
+                        />
+                        <input
+                            type="text"
+                            value={editedAddress.country || address.country}
+                            onChange={e => setEditedAddress({...editedAddress, country: e.target.value})}
+                        />
+                    </div>
+                ) : (
+                    
+                    <div>
+                        <p>Street: {address.street}</p>
+                        <p>City: {address.city}</p>
+                        <p>Postal Code: {address.zipCode}</p>
+                        <p>Country: {address.country}</p>
+                    </div>
+                )}
+                {editingAddressId === address.id ? (
+                         <button onClick={() => saveAddress(address.id)}>Save</button>
+                     ) : (
+                         <button onClick={() => startEditing(address)}>Edit</button>
+                     )}
+                     -
+                     <button onClick={() => handleDeleteAddress(address.id)}>Delete</button>
+                </div>
+            ))}
+        </div>
+
             <div className="card-column">
+                {/* addresses part  */}
+            <form onSubmit={handleAddAddress} className='address-form'>
+                <h3>Adresses</h3>
+                <div>
+                    <label htmlFor="street">Street:</label>
+                    <input
+                        type="text"
+                        id="street"
+                        name="street"
+                        value={newAddress.street}
+                        onChange={e => setNewAddress({...newAddress, street: e.target.value})}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="city">City:</label>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        value={newAddress.city}
+                        onChange={e => setNewAddress({...newAddress, city: e.target.value})}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="zipCode">Postal Code:</label>
+                    <input
+                        type="text"
+                        id="zipCode"
+                        name="zipCode"
+                        value={newAddress.zipCode}
+                        onChange={e => setNewAddress({...newAddress, zipCode: e.target.value})}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="country">Country:</label>
+                    <input
+                        type="text"
+                        id="country"
+                        name="country"
+                        value={newAddress.country}
+                        onChange={e => setNewAddress({...newAddress, country: e.target.value})}
+                    />
+                </div>
+            </form>
+            <button type="submit" className='address-btn'>Add this address</button>
+
+
             <button onClick={toggleChangePasswordForm}>Change Password</button>
             {showChangePasswordForm && (
                     <form onSubmit={handleSubmitPasswordChange}>
@@ -285,7 +472,7 @@ const UserInfo = () => {
         {Message && <p className={`${messageType}-message`}>{Message}</p>}
 
     <div>
-    <button onClick={handleLogout} className="logout-btn">Log Out</button> {/* Ajout du bouton de déconnexion */} - {/* Bouton pour supprimer le compte */}
+    <button onClick={handleLogout} className="logout-btn">Log Out</button> {/* Ajout du bouton de déconnexion */} {/* Bouton pour supprimer le compte */}
                 <button onClick={handleDeleteAccount} className="delete-account-btn">
                     Delete Account
                 </button>
